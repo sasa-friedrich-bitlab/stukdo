3c98cb7f173f827e48e2170a36e174144e46a9bb
Saša Friedrich <sasa.friedrich@bitlab.si>
2015-02-27 13:31:21 +0100

    state_links helper method refactor

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -5,10 +5,10 @@ module TasksHelper
     when "to_do"
       link_to "<i class='fa fa-arrow-right'></i>".html_safe, change_task_path(task, state: "doing"), method: :put, class: "btn btn-block btn-info"
     when "doing"
-      html = "<div class='btn-group btn-group-justified'>"
-      html << link_to("<i class='fa fa-arrow-left'></i>".html_safe, change_task_path(task, state: "to_do"), method: :put, class: "btn btn-info")
-      html << link_to("<i class='fa fa-arrow-right'></i>".html_safe, change_task_path(task, state: "done"), method: :put, class: "btn btn-info")
-      html << "</div>"
+      html =  content_tag(:div, class: "btn-group btn-group-justified") do
+                concat link_to "<i class='fa fa-arrow-left'></i>".html_safe, change_task_path(task, state: "to_do"), method: :put, class: "btn btn-info"
+                concat link_to "<i class='fa fa-arrow-right'></i>".html_safe, change_task_path(task, state: "done"), method: :put, class: "btn btn-info"
+              end
       html.html_safe
     when "done"
       link_to "<i class='fa fa-arrow-left'></i>".html_safe, change_task_path(task, state: "doing"), method: :put, class: "btn btn-block btn-info"
